fa95ae3ec5ae6a7c41b79557349fa170a821a758
Rohan Sethi <rsethi1@luc.edu>
2023-01-15 09:15:09 -0600

    Added extractFeatures psuedocode.

---
+++ src/features/extractFeatures.py
@@ -0,0 +1,5 @@
+# import TF library with pretrained CNNs
+
+# function with arguments like what CNN wanting to use, preprocessed images directory, output feature directory
+
+    # execute function to save feature vectors into the output directory as a TFRecords file
